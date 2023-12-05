e6046b44a7c86f03b3173be619e4906f9e384338
Vipin Mallaya <vkgopinathan@live.com>
2023-12-05 16:23:29 +0100

    Add project files.

---
+++ Helpers/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using AssessmentVipin.Core.Helpers.Abstraction;
+using System.Configuration;
+
+namespace AssessmentVipin.Helpers
+{
+    public class Configuration : IConfiguration
+    {
+        public string BaseUrl
+        {
+            get;
+            private set;
+        }
+
+        public string Token
+        {
+            get;
+            private set;
+        }
+
+        public Configuration()
+        {
+            BaseUrl = ConfigurationManager.AppSettings["BaseUrl"].ToString();
+            Token = ConfigurationManager.AppSettings["Token"].ToString();
+        }
+    }
+}
---
+++ Views/NewEmployeeWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using AssessmentVipin.Core.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace AssessmentVipin.Views
+{
+    /// <summary>
+    /// Interaction logic for NewEmployeeWindow.xaml
+    /// </summary>
+    public partial class NewEmployeeWindow : Window
+    {
+        public NewEmployeeWindow()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService<NewEmployeeViewModel>();
+        }
+    }
+}
